ffd1694a4ae0d16e0c515c89b6e7fd78e6b6e556
fc-j <francisco.brokz@gmail.com>
2025-04-13 08:23:16 -0500

    realzando cambios

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main (){
          
     }
     
-
+    
 
 
     for(int num : enteros){
--- timeConversion.cpp
+++ timeConversion.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <string>
 using namespace std; 
 int main (){
-    string  s; 
+    string  s, z; 
     getline(cin, s); 
 
-    cout<<s
+    //10:09:12pm,
+    int len=s.length(), hora=0; 
+    char flag=s.at(len-2);
+    
+    
+    
+    
+    int d = stoi(s.substr(0,2)); 
+    if(flag=='P'||flag=='p'){   
+        if(d<12){
+            cout<<12+d<<s.substr(2,len-4);
+        }
+        else{
+            cout<<s.substr(0,len-2); 
+        }
+    }else{
+        if(d==12){
+            cout<<"00"<<s.substr(2,len-4); 
+
+        }
+        else{
+            cout<<s.substr(0,len-2); 
+          
+        }
+    }        
 
 
-
-}
+    
+}   
